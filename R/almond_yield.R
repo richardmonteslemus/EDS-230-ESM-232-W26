37b4a3ddb78a57ceed0d33bd5beceb4258338eb0
richardmonteslemus <richard.montes.lemus@gmail.com>
2026-01-29 10:42:01 -0800

    SOBOL practice

--- R/almond_yield.R
+++ R/almond_yield.R
@@ -1,15 +1,23 @@
-#' Model to determing projected almond yield based on climate parameters
+#' Almond yield anomaly model based on Lobell et al. (2006)
+#' 
+#' Model to determining projected almond yield based on climate parameters
 #'
-#' @param tmin_feb Vector of minimum temperatures in February per year.
-#' @param precip_jan Vector of total precipitation in January per year.
+#' @param tmin_feb Numeric vector of minimum temperatures in February per year.
+#' @param precip_jan Numeric vector of total precipitation in January per year.
 #'
-#' @returns
+#' @returns Numeric vector of annual almond yield anomalies (ton/acre).
 #' @export
 #'
-#' @examples almond_yield(5.6, 10)
+#' @examples almond_yield(tmin_feb = 5.6, precip_jan = 10)
 
 almond_yield <- function(tmin_feb, precip_jan) {
-  yield = -0.015*tmin_feb - 0.0046 * (tmin_feb)^2 - 0.07 * precip_jan + 0.0043 * (precip_jan)^2 + 0.28
+  
+  yield = (-0.015 * tmin_feb) - 
+    (0.0046 * (tmin_feb)^2) - 
+    (0.07 * precip_jan) + 
+    (0.0043 * (precip_jan)^2) + 
+    0.28
   
   return(yield)
+  
 }
